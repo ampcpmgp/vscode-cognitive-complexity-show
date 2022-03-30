dc3304dc8d791b8d9e6f34acfc525e9f7c1dc1d7
ampcpmgp <ampcpmgp@gmail.com>
2022-03-30 15:11:37 +0900

    add execute command

--- extension.js
+++ extension.js
@@ -1,36 +1,30 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-const vscode = require('vscode');
-
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+const { window, commands, ExtensionContext, TextDocument } = require("vscode");
 
 /**
- * @param {vscode.ExtensionContext} context
+ * @param {ExtensionContext} context
  */
 function activate(context) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "cognitive-complexity-show" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('cognitive-complexity-show.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from cognitive-complexity-show!');
-	});
-
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(
+    commands.registerCommand("cognitive-complexity-show.execute", () => {
+      const document = window.activeTextEditor?.document;
+      if (document) processActiveFile(document);
+    })
+  );
 }
 
-// this method is called when your extension is deactivated
+/**
+ *
+ * @param {TextDocument} document
+ */
+async function processActiveFile(document) {
+  console.log("cognitive-complexity-show", document);
+
+  window.showInformationMessage("Hello World from cognitive-complexity-show!");
+}
+
 function deactivate() {}
 
 module.exports = {
-	activate,
-	deactivate
-}
+  activate,
+  deactivate,
+};
